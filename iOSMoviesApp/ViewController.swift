9ab5fd8676b70aae4ff42a87e241877b2335357b
yash.agrawal <yash.agrawal@bookmyshow.com>
2025-07-07 15:17:33 +0530

    Initial Commit

---
+++ iOSMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSMoviesApp
+//
+//  Created by Yash Agrawal on 07/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
