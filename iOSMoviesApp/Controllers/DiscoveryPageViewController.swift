3fb6f8e1f99fa591adaa8735fa6c771ddc295bd0
yash.agrawal <yash.agrawal@bookmyshow.com>
2025-07-08 17:56:24 +0530

    Merged discovery and home, merged DiscoveryVM and MovieListVM, refactored Discovery Controller code

--- iOSMoviesApp/Controllers/DiscoveryPageViewController.swift
+++ iOSMoviesApp/Controllers/DiscoveryPageViewController.swift
@@ -7,23 +7,34 @@
 
 import Foundation
 import UIKit
+protocol DiscoveryPageViewControllerDelegate: AnyObject{
+    func reloadTableView()
+    func reloadCarouselView()
+}
 
-class DiscoveryPageViewController: UIViewController {
-
+class DiscoveryPageViewController: UIViewController{
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var movieListVM = MovieListViewModel()
+    
     @IBOutlet weak var carouselCollectionView: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
 
-    let viewModel = DiscoveryViewModel()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
+    }
+    private func setupUI() {
+        movieListVM.fetchMovies(delegate: self)
+        setupTableView()
         setupSearchBar()
         setupCarousel()
-        fetchData()
     }
-    
-
+    func setupTableView() {
+        self.tableView.register(UINib(nibName: "MovieCardTableViewCell", bundle: nil), forCellReuseIdentifier: "MovieCardTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 
     private func setupSearchBar() {
         searchBar.delegate = self
@@ -44,20 +55,20 @@ class DiscoveryPageViewController: UIViewController {
         carouselCollectionView.showsHorizontalScrollIndicator = false
     }
 
-    private func fetchData() {
-        viewModel.fetchMovies { [weak self] in
-            DispatchQueue.main.async {
-                self?.carouselCollectionView.reloadData()
-            }
-        }
-    }
+//    private func fetchData() {
+//        viewModel.fetchMovies { [weak self] in
+//            DispatchQueue.main.async {
+//                self?.carouselCollectionView.reloadData()
+//            }
+//        }
+//    }
 
 }
 
 extension DiscoveryPageViewController: UICollectionViewDataSource, UICollectionViewDelegate {
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.featuredMovies.count
+        return movieListVM.numberOfItems()
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -65,16 +76,19 @@ extension DiscoveryPageViewController: UICollectionViewDataSource, UICollectionV
             return UICollectionViewCell()
         }
 
-        let movie = viewModel.featuredMovies[indexPath.row]
-        cell.configure(with: movie)
+//        let movie = viewModel.featuredMovies[indexPath.row]
+        let movieVM = movieListVM.getMovieViewModel(at: indexPath)
+        cell.configure(with: movieVM)
         return cell
     }
 }
 
 extension DiscoveryPageViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        viewModel.filterMovies(query: searchText)
-        carouselCollectionView.reloadData()
+//        viewModel.filterMovies(query: searchText)
+//        carouselCollectionView.reloadData()
+        movieListVM.initializeSearch(for: searchText)
+        self.reloadCarouselView()
     }
 
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
@@ -84,9 +98,38 @@ extension DiscoveryPageViewController: UISearchBarDelegate {
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.text = ""
         searchBar.resignFirstResponder()
-        viewModel.filterMovies(query: "")
-        carouselCollectionView.reloadData()
+//        viewModel.filterMovies(query: "")
+        movieListVM.initializeSearch(for: nil)
+        self.reloadTableView()
+        self.reloadCarouselView()
         searchBar.setShowsCancelButton(false, animated: true)
     }
 }
 
+extension DiscoveryPageViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieListVM.numberOfMovies()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellVM = self.movieListVM.getMovieViewModel(at: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCardTableViewCell", for : indexPath) as? MovieCardTableViewCell
+        guard let cell = cell else { return UITableViewCell() }
+        cell.configureState(for : cellVM)
+        return cell
+    }
+}
+
+extension DiscoveryPageViewController : DiscoveryPageViewControllerDelegate {
+   func reloadTableView() {
+       DispatchQueue.main.async{
+           self.tableView.reloadData()
+       }
+   }
+    func reloadCarouselView() {
+        DispatchQueue.main.async{
+            self.carouselCollectionView.reloadData()
+        }
+    }
+}
--- iOSMoviesApp/Controllers/MovieListViewController.swift
+++ iOSMoviesApp/Controllers/MovieListViewController.swift
@@ -1,41 +1,46 @@
+////
+////  MovieListViewController.swift
+////  iOSMoviesApp
+////
+////  Created by Yash Agrawal on 08/07/25.
+////
 //
-//  MovieListViewController.swift
-//  iOSMoviesApp
+//import Foundation
+//import UIKit
+//protocol MovieListViewControllerDelegate: AnyObject {
+//    func reloadTableView()
+//}
+//class MovieListTableViewController: UITableView {
 //
-//  Created by Yash Agrawal on 08/07/25.
+//    private var movieListVM = MovieListViewModel()
+//    
+////    override func viewDidLoad() {
+////        super.viewDidLoad()
+////        setupTableView()
+////        movieListVM.fetchMovies(delegate: self)
+////    }
+////    func setupTableView() {
+////        self.tableView.register(UINib(nibName: "MovieCardTableViewCell", bundle: nil), forCellReuseIdentifier: "MovieCardTableViewCell")
+////        
+////    }
+//}
 //
-
-import Foundation
-import UIKit
-protocol MovieListViewControllerDelegate: AnyObject {
-    func reloadTableView()
-}
-class MovieListViewController: UIViewController {
-    
-    @IBOutlet weak var tableView: UITableView!
-    private var movieListVM: MovieListViewModel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-}
-
-// MARK: - UITableViewDelegate & UITableViewDataSource
-extension MovieListViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieListVM.numberOfMovies()
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellVM = self.movieListVM.getMovieViewModel(at: indexPath)
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCardTableViewCell", for : indexPath) as? MovieCardTableViewCell
-        guard let cell = cell else { return UITableViewCell() }
-        cell.configureState(for : cellVM)
-        return cell
-    }
-}
-extension MovieListViewController: MovieListViewControllerDelegate {
-    func reloadTableView() {
-        self.tableView.reloadData()
-    }
-}
+//// MARK: - UITableViewDelegate & UITableViewDataSource
+//extension MovieListTableViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return movieListVM.numberOfMovies()
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cellVM = self.movieListVM.getMovieViewModel(at: indexPath)
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCardTableViewCell", for : indexPath) as? MovieCardTableViewCell
+//        guard let cell = cell else { return UITableViewCell() }
+//        cell.configureState(for : cellVM)
+//        return cell
+//    }
+//}
+//extension MovieListTableViewController: MovieListViewControllerDelegate {
+//    func reloadTableView() {
+//        self.tableView.reloadData()
+//    }
+//}
