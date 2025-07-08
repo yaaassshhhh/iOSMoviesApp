65bffd185a3becead0d65ac915d22972cecf51c0
Tarang Sultania <tarang.sultania@bookmyshow.com>
2025-07-08 11:53:15 +0530

    started working on header section

---
+++ iOSMoviesApp/Controllers/DiscoveryPageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DiscoveryPageViewController.swift
+//  iOSMoviesApp
+//
+//  Created by Tarang Sultania on 08/07/25.
+//
+
+import Foundation
+import UIKit
+
+class DiscoveryPageViewController: UIViewController {
+    
+}
