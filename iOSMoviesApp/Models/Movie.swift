08fe5596f897c3bd1e9fd8e407981263111fbe45
yash.agrawal <yash.agrawal@bookmyshow.com>
2025-07-09 14:18:14 +0530

    LoadImage moved to MovieViewModel

--- iOSMoviesApp/Models/Movie.swift
+++ iOSMoviesApp/Models/Movie.swift
@@ -10,15 +10,15 @@ import Foundation
 struct Movie: Decodable {
     var title: String
     var releaseDate : String
-    var posterURL: String
+    var posterPath: String
     var description: String
     var id: Int
     
     enum CodingKeys: String, CodingKey {
         case title = "original_title"
         case releaseDate = "release_date"
-        case posterURL = "poster_path"
         case id
+        case posterPath  = "poster_path"
         case description = "overview"
     }
 }
