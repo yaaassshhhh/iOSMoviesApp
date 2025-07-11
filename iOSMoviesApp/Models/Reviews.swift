d9da32f27c5813bb090ed7cc1aebb7a947236ff3
Tarang Sultania <tarang.sultania@bookmyshow.com>
2025-07-11 12:15:49 +0530

    initializd detauls page and review section

---
+++ iOSMoviesApp/Models/Reviews.swift
@@ -0,0 +1,13 @@
+//
+//  Reviews.swift
+//  iOSMoviesApp
+//
+//  Created by Tarang Sultania on 11/07/25.
+//
+
+import Foundation
+
+struct Review : Decodable{
+    let name: String
+    let comment: String
+}
