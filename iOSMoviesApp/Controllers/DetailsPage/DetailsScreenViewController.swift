c0ff5bdc1f54bbe6117e410e45d93b08458fe7b3
Tarang Sultania <tarang.sultania@bookmyshow.com>
2025-07-11 12:15:49 +0530

    initializd detauls page and review section

--- iOSMoviesApp/Controllers/DetailsPage/DetailsScreenViewController.swift
+++ iOSMoviesApp/Controllers/DetailsPage/DetailsScreenViewController.swift
@@ -8,6 +8,34 @@
 import Foundation
 import UIKit
 
-class DetailsScreenViewController: UIViewController {
+class DetailsScreenViewController: UIViewController, UITableViewDataSource {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UINib(nibName: "ReviewSectionCell", bundle: nil), forCellReuseIdentifier: "ReviewSectionCell")
+
+        tableView.estimatedRowHeight = 300
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+}
+
+extension DetailsScreenViewController: UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
