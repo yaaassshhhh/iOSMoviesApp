4f3a4aac834ad07ac7585e7cbb47d9f4ee390c82
yash.agrawal <yash.agrawal@bookmyshow.com>
2025-07-11 14:49:51 +0530

    Separated View Models into respective Files for readability

--- iOSMoviesApp/ViewModels/DetailsScreenViewModel.swift
+++ iOSMoviesApp/ViewModels/DetailsScreenViewModel.swift
@@ -37,42 +37,4 @@ extension DetailsScreenViewModel {
         return self.casts[index]
     }
 }
-struct castViewModel {
-    var cast : Cast
-    var posterBaseURL: String = "https://image.tmdb.org/t/p/w92"
-    init(cast : Cast){
-        self.cast = cast
-    }
-}
-extension castViewModel : Identifiable {
-    var id : Int {
-        return self.cast.id
-    }
-    var posterPath : String {
-        return self.posterBaseURL + self.cast.posterPath
-    }
-    var realName : String {
-        return self.cast.realName
-    }
-    var fictionalName : String {
-        return self.cast.fictionalName
-    }
-    
-//    func loadCastImage(delegate: castCollectionViewCellDelegate?) {
-//        guard let delegate = delegate else { return }
-//        guard let imageURL = URL(string: self.posterPath) else { return }
-//        let cacheKey = NSString(string: self.posterPath)
-//        
-//        if let cachedImage = CastImageCache.shared.object(forKey: cacheKey) {
-//            DispatchQueue.main.async {
-//                delegate.updateCastPosterFromCache(with: cachedImage)
-//            }
-//            return
-//        }
-//        
-//        DispatchQueue.global(qos: .userInitiated).async {
-//            guard let imageData = try? Data(contentsOf: imageURL) else { return }
-//            delegate.updateCastPoster(with: imageData, cacheKey: cacheKey)
-//        }
-//    }
-}
+
