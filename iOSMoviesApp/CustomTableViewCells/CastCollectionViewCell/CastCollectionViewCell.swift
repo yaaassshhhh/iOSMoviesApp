e1677f061fa728fa04cbee33d32661015040d41b
yash.agrawal <yash.agrawal@bookmyshow.com>
2025-07-11 14:23:21 +0530

    DetailsScreenViewModel and  Cast model implemented
    "Conflict resoltion"

---
+++ iOSMoviesApp/CustomTableViewCells/CastCollectionViewCell/CastCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CastCollectionViewCell.swift
+//  iOSMoviesApp
+//
+//  Created by Yash Agrawal on 11/07/25.
+//
+
+import UIKit
+
+class CastCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var realName: UILabel!
+    @IBOutlet weak var fictionalName: UILabel!
+    @IBOutlet weak var castImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
