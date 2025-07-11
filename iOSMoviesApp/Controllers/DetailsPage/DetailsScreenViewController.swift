d32d7df43bec59f1bbd0a0feba748bc5fbf4b545
yash.agrawal <yash.agrawal@bookmyshow.com>
2025-07-11 11:18:54 +0530

    Structured controllers into respective folders

---
+++ iOSMoviesApp/Controllers/DetailsPage/DetailsScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsScreenViewController.swift
+//  iOSMoviesApp
+//
+//  Created by Yash Agrawal on 11/07/25.
+//
+
+import Foundation
+import UIKit
+
+class DetailsScreenViewController: UIViewController {
+    
+}
