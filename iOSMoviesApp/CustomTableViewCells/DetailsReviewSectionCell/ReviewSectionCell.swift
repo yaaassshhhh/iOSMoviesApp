16df591b49b42536e16ddf49808a9892128efec5
Tarang Sultania <tarang.sultania@bookmyshow.com>
2025-07-11 12:15:49 +0530

    initializd detauls page and review section

---
+++ iOSMoviesApp/CustomTableViewCells/DetailsReviewSectionCell/ReviewSectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewSectionCell.swift
+//  iOSMoviesApp
+//
+//  Created by Tarang Sultania on 11/07/25.
+//
+
+import UIKit
+
+class ReviewSectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
