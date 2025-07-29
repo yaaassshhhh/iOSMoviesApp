9626440f6c4cf8a9c05a949c9a6ad6ba7a64349b
Tarang Sultania <tarang.sultania@bookmyshow.com>
2025-07-30 01:57:40 +0530

    initialized screen view controller

---
+++ iOSMoviesApp/Controllers/SearchScreenViewController/SearchScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchScreenViewController.swift
+//  iOSMoviesApp
+//
+//  Created by Tarang Sultania on 30/07/25.
+//
+
+import UIKit
+
+class SearchScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
