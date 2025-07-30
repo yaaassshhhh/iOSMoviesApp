819ac9a713e8290bff8584c8fcdfaf032761b2a3
Tarang Sultania <tarang.sultania@bookmyshow.com>
2025-07-30 12:36:05 +0530

    edited the soryboard

--- iOSMoviesApp/Controllers/SearchScreenViewController/SearchScreenViewController.swift
+++ iOSMoviesApp/Controllers/SearchScreenViewController/SearchScreenViewController.swift
@@ -10,10 +10,58 @@ import UIKit
 class SearchScreenViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var searchText: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.dataSource = self
+        tableView.delegate = self
         
     }
+}
+
+extension SearchScreenViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let position : Int = indexPath.row
+        
+        switch position {
+            case 0:
+                let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "SearchResultCell", for: indexPath)
+            return cell
+        default:
+            fatalError("Invalid position")
+        }
+    }
+    
+    
+    
+}
+
+extension SearchScreenViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.searchText = searchText
+//        movieListVM.initializeSearch(for: searchText)
+//        self.reloadTableData()
+    }
+
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+//        movieListVM.initializeSearch(for: searchBar.text)
+//        self.reloadTableData()
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
+        
 }
---
+++ iOSMoviesApp/CustomTableViewCells/SearchTitleTableViewCell/SearchTitleTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SearchTitleTableViewCell.swift
+//  iOSMoviesApp
+//
+//  Created by Tarang Sultania on 30/07/25.
+//
+
+import UIKit
+
+class SearchTitleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var searchTitleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
